fdbbf48efb73001838533981ddfd51c655b7ea0d
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-06-10 15:48:14 +0100

    Calculator completed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,27 @@
 public class Calculator {
 
-    private double 
+    private double firstNumber;
+    private double secondNumber;
+
+    public Calculator(double firstNumber, double secondNumber){
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double add(){
+        return (firstNumber + secondNumber);
+    }
+
+    public double subtract(){
+        return (firstNumber - secondNumber);
+    }
+
+    public double multiply(){
+        return (firstNumber * secondNumber);
+    }
+
+    public double divide(){
+        return (firstNumber / secondNumber);
+    }
 
 }
---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator(12.00, 9.00);
+    }
+
+    @Test
+    public void theCalculatorCanAddTwoNumbers(){
+        assertEquals(21.00, calculator.add());
+    }
+
+    @Test
+    public void theCalculatorCanSubtractTwoNumbers(){
+        assertEquals(3.00, calculator.subtract());
+    }
+
+    @Test
+    public void theCalculatorCanMultiplyTwoNumbers(){
+        assertEquals(108.00, calculator.multiply());
+    }
+
+    @Test
+    public void theCalculatorCanDivideTwoNumbers(){
+        assertEquals(1.33, calculator.divide(), 0.01);
+    }
+}
